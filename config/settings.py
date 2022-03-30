a08876a18112f5e2f82ae5e8ee06edb925f58181
takumi <y.takumi4@gmail.com>
2022-03-31 00:00:29 +0900

    staticを編集

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+'''
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
 ]
+'''
 STATIC_ROOT = '/usr/share/nginx/html/static'
 
 # Default primary key field type
