b8e39cbeecf08a4094a85595fff262a20f140535
takumi <y.takumi4@gmail.com>
2022-04-15 01:06:48 +0900

    urlパスルーティングを変更

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('',include('ShiftManagementApp.urls')),
     path('admin/', admin.site.urls),
-    path('ShiftManagementApp/',include('ShiftManagementApp.urls')),
+    #path('ShiftManagementApp/',include('ShiftManagementApp.urls')),
 ]
