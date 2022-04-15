a80f741ab1a3f9b4d2745cbc63692438032652c1
takumi <y.takumi4@gmail.com>
2022-04-15 16:39:17 +0900

    一時的にパスワードバリテータを解除

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+'''
 
 
 # Internationalization
