fffa3821223f88e32e63a3ddc9e8dba23f748842
takumi <y.takumi4@gmail.com>
2022-05-21 17:54:44 +0900

    削除リクエストの際も編集モードかどうか判定する #43

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -310,6 +310,7 @@ def editshift(request):
         return HttpResponse('アクセス権がありません')
 
 '''
+【シフト編集画面】
 シフト編集画面の日付を入力したときの送信先
 シフト編集画面描画用のシフトデータをjsonで返す
 '''
@@ -360,6 +361,7 @@ def editshift_ajax(request):
 
 
 '''
+【シフト編集画面】
 シフトデータを新規追加or編集したときの送信先
 '''
 @login_required
@@ -432,8 +434,11 @@ def editshift_ajax_delete_shiftdata(request):
 
     #削除リクエストが一般ユーザーの場合、編集可能期間かどうかで可否を変える
     else:
-        #編集可能期間かどうか判定
-        if Judge_editable(datas['start']):
+        """
+        削除リクエストの判定
+        編集可能期間もしくは、編集モードの時に削除リクエストを受け付ける
+        """
+        if (Judge_editable(datas['start']) == True or request.user.is_edit_mode == True):
             #getは対象が存在しないと例外を返すため念の為try文にしている
             try:
                 Shift.objects.get(id=datas['id']).delete()
